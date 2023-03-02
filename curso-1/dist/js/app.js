fd75a887d54c6f5e8c6e157a66823f9909046bf4
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2023-03-02 20:47:42 -0300

    adicionando aula 1 curso 1

---
+++ curso-1/dist/js/app.js
@@ -0,0 +1,9 @@
+import { Negociacao } from "./models/Negociacao.js";
+
+const minhaNegociacao = new Negociacao(Date.now(), 10, 1000);
+console.log(
+    minhaNegociacao.data,
+    minhaNegociacao.quantidade,
+    minhaNegociacao.valor,
+    minhaNegociacao.volume
+);
